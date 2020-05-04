08c3303f6a99cf806c55898447bedf8e9f6e95fe
Herbert Lange <herbert.lange@cse.gu.se>
2020-05-04 10:40:48 +0200

    change order of parameters

--- Main.hs
+++ Main.hs
@@ -22,10 +22,10 @@ usage =
     putStrLn "\twav-file: the output file to be written"
     putStrLn "\tletter rate: optional speed of letters in characters per minute"
     putStrLn "\tbreak rate: optional speed of breaks in characters per minute"
+    putStrLn "\ttxt-file: optional output file for the text"
     putStrLn "\tletters: optional list of letters that should be practiced."
     putStrLn "\t\tIf this list contains punctuation or digits,"
     putStrLn "\t\tthey will be randomly generated between words"
-    putStrLn "\ttxt-file: optional output file for the text"
 
 main :: IO ()
 main =
@@ -37,9 +37,9 @@ main =
         let ct = read (args !! 1)
         let letter_rate = if length args >= 4 then (read $ args !! 3) else rate
         let break_rate = if length args >= 5 then (read $ args !! 4) else rate
-        let ltrs = if length args >= 6 then (args !! 5) else letters
+        let ltrs = if length args == 7 then (args !! 6) else letters
         dict <- loadDict (args !! 0 ++ ".txt") ltrs
         let wav = args !! 2
         text <- sampleText dict ltrs ct
         textToMorse letter_rate break_rate text wav
-        if length args == 7 then writeFile (args !! 6) text else return ()
+        if length args >= 6 then writeFile (args !! 5) text else return ()
